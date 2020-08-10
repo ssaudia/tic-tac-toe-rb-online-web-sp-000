1d0d6842fc551fddbb7b999e3ba8f514a4afe5ea
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-10 23:20:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,8 @@ def play(board)
   end
 
   if won?(board)
-    puts ("Congratulations X!")
+    puts ("Congratulations X!") ||
+    puts ("Congratulations O!")
   else
     puts "Cat's Game!"
   end
